b2acb1e45043109daabe199ea4dd9070f53f0f5f
Rayane <soulaimanrayane2@gmail.com>
2026-08-12 23:29:55 +0200

    Logo et benier de texte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Ray-shop | Votre Caisse Intelligente</title>
+        <title>Camy-shop | Votre Caisse Intelligente</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,600,800" rel="stylesheet" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
