cb857f14c6e0c5891b537d3e75586d2ea53742e0
Rayane <soulaimanrayane2@gmail.com>
2026-08-13 10:29:31 +0200

    Ajout du texte libraire camy

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center gap-3">
-    <span class="text-xl font-black tracking-tighter uppercase text-gray-900">Camy-<span class="text-blue-600">Shop</span></span>
+    <span class="text-xl font-black tracking-tighter uppercase text-gray-900">Librairie-<span class="text-blue-600">Camy</span></span>
 </div>
